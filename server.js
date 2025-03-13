94c5555f40238cf800c4f468b9c956edc4b1863f
Breyner794 <jhoanquilindo@gmail.com>
2025-03-13 15:41:37 -0500

    Nueva ruta agregada user.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 import serviceRoutes from "./src/routes/service.routes.js";
 import siteRoutes from "./src/routes/site.routes.js";
 import barberRoutes from "./src/routes/barber.routes.js";
+import userRoutes from "./src/routes/user.routes.js";
 
 dotenv.config();
 const app = express();
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use("/api/services", serviceRoutes);
 app.use("/api/site", siteRoutes);
 app.use("/api/barber", barberRoutes);
+app.use("/api/user", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("API del sistema de servicios esta funcionando");
