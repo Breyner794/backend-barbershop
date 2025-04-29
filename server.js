81682f77aa0b0f1bf690f80a064e19cf889fd183
Breyner794 <jhoanquilindo@gmail.com>
2025-04-29 15:41:32 -0500

    se configura la ruta de la auth, y se agrega nuevas dependencias como jwt y bcrypt

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import cookieParser from 'cookie-parser';
 import { connectDB } from "./server/db/connection.js";
 import dotenv from "dotenv";
 import serviceRoutes from "./src/routes/service.routes.js";
@@ -8,6 +9,7 @@ import siteRoutes from "./src/routes/site.routes.js";
 import barberRoutes from "./src/routes/barber.routes.js";
 import userRoutes from "./src/routes/user.routes.js";
 import reservationRoutes from "./src/routes/reservation.routes.js"
+import authRoutes from "./src/routes/auth.routes.js"
 
 dotenv.config();
 const app = express();
@@ -19,12 +21,22 @@ const PORT = process.env.PORT || 5050;
 app.use(cors());
 
 app.use(express.json());
+app.use(cookieParser());
 
+app.use('/api/auth', authRoutes);
 app.use("/api/services", serviceRoutes);
 app.use("/api/site", siteRoutes);
 app.use("/api/barber", barberRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/reservation", reservationRoutes);
+
+// Middleware para manejar rutas no encontradas
+app.all('*', (req, res) => {
+  res.status(404).json({
+    status: 'error',
+    message: `No se encontró la ruta: ${req.originalUrl}`
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("API del sistema de servicios esta funcionando");
