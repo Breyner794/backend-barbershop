715356a6397d0643eaef030ed284e1f22fcf3a50
Breyner794 <jhoanquilindo@gmail.com>
2025-06-09 13:28:23 -0500

    Se comentan las rutas protegidas del middleware, para hacer pruebas, por el momento funciona normalmente y se agrega la nueva ruta.

--- src/routes/site.routes.js
+++ src/routes/site.routes.js
@@ -6,17 +6,19 @@ import {
   getAllSite,
   getByIdSite,
   updateSite,
+  getBarbersBySite
 } from "../controllers/site.controller.js";
 
 const router = express.Router();
 
 router.get("/", getAllSite);
 
-router.use(protect);
+//router.use(protect);
 
-router.use(restrictTo('admin','superadmin'));
+//router.use(restrictTo('admin','superadmin'));
 
 router.get("/:id", getByIdSite);
+router.get('/:siteId/barbers', getBarbersBySite);
 router.post("/", createSite);
 router.put("/:id", updateSite);
 router.delete("/:id", deleteSite);
