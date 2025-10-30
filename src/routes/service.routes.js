991c38a7a825e369ddd1e7e1ab24088f3e8ef4fc
Breyner794 <jhoanquilindo@gmail.com>
2025-10-30 14:58:43 -0500

    refactor: Incluir proteccion a la api /dashboard-services.

--- src/routes/service.routes.js
+++ src/routes/service.routes.js
@@ -3,7 +3,6 @@ import { protect, restrictTo } from "../controllers/auth.Controller.js";
 import {
   getAllServices,
   getServicesDashboard,
-  getByIdServices,
   createServices,
   updateServices,
   deleteServices,
@@ -13,11 +12,11 @@ import { uploadServiceImage } from "../../utils/multerConfig.js";
 const router = express.Router();
 
 router.get("/", getAllServices);
-router.get("/dashboard-services", getServicesDashboard)
-router.get("/:id", getByIdServices);
 
 router.use(protect);
 
+router.route("/dashboard-services")
+        .get(restrictTo("admin", "superadmin", "barbero"), getServicesDashboard)
 router.route("/")
         .post(restrictTo("admin", "superadmin"),uploadServiceImage, createServices)
 router.route("/:id")
